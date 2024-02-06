96fe218535940da11f5c888ff4a7427be9da8e66
devtea <devtea@protonmail.com>
2024-02-06 19:07:02 +0700

    rework cli params argument

--- bin/cli.js
+++ bin/cli.js
@@ -10,26 +10,18 @@ const params = program
     .name('codegen-openapi-ts-cli')
     .usage('[options]')
     .version(pkg.version)
-    .argument('<from>', 'Original response specification version')
     .argument('<source>', 'Swagger/OpenAPI response url')
-    .argument('[output]', 'Output folder name', 'output')
+    .argument('<output>', 'Output folder name')
     .parse(process.argv)
     .processedArgs;
 
 const OpenAPI = require(path.resolve(__dirname, '../dist/index.js'));
 
 if (OpenAPI) {
-    OpenAPI.convertAndGenerate(
-      {
-        from: params[0],
-        to: 'openapi_3',
-        source: params[1]
-      },
-      {
-        input: 'api-schema.json',
-        output: params[2],
-        useOptions: true,
-        useUnionTypes: true
-      },
-    )
+    OpenAPI.generate({
+      input: params[0],
+      output: params[1],
+      useOptions: true,
+      useUnionTypes: true 
+    })
 }
